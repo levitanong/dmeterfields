c5fec7c553f7748a410eb839dda1fc7b0f66cd0b
Levi Tan Ong <levi@byimplication.com>
2016-11-15 02:58:57 +0800

    attempts deeper usage of jgit

--- src/clj/github_deploy/boot_github_deploy.clj
+++ src/clj/github_deploy/boot_github_deploy.clj
@@ -6,7 +6,11 @@
    [clj-jgit.porcelain :as git]
    [clj-jgit.querying :as git-query]
    [clojure.java.io :as io]
-   [clojure.set :as set]))
+   [clojure.set :as set])
+  (:import
+   (org.eclipse.jgit.transport
+     JschConfigSessionFactory
+     SshSessionFactory)))
 
 (def repo-states #{:added :changed :missing :modified :removed :untracked})
 
@@ -16,8 +20,19 @@
         current-branch (git/git-branch-current repo)
         latest-commit (first (git/git-log repo))
         status (git/git-status repo)
-        clean? (->> status vals (reduce set/union) empty?)]
-    (if-not clean?
+        clean? (->> status vals (reduce set/union) empty?)
+        session-factory (configure (JschConfigSessionFactory.)
+                          [hc session]
+                          (.setPassword session "passphrase"))]
+    (SshSesssionFactory/setInstance session-factory)
+    (-> repo
+      .push
+      (.setRemote "origin")
+      .call)
+    #_(git/with-identity {:passphrase 
+                        :public (slurp "~/.ssh/id_rsa.pub")}
+      (println "hi"))
+    #_(if-not clean?
       (util/fail (str "Repo not clean: " status))
       (do
         (util/info "Repo is clean. Checking out master.")
