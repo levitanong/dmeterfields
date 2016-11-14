3a8c62cb2503b037fb9abd76097e5cdd7c8369d5
Levi Tan Ong <levi@byimplication.com>
2016-11-15 00:22:50 +0800

    adds guard against unclean repo

--- src/clj/github_deploy/boot_github_deploy.clj
+++ src/clj/github_deploy/boot_github_deploy.clj
@@ -2,11 +2,20 @@
   {:boot/export-tasks true}
   (:require
    [boot.core :as boot :refer [deftask]]
+   [boot.util :as util]
    [clj-jgit.porcelain :as git]))
+
+(def repo-states #{:added :changed :missing :modified :removed :untracked})
 
 (deftask github-deploy
   []
   (let [repo (git/load-repo ".")
-        current-branch (git/git-branch-current repo)]
-    (println (git/git-status repo))))
+        current-branch (git/git-branch-current repo)
+        status (git/git-status repo)
+        clean? (every? (fn [state] (empty? (get status state)))
+                 repo-states)]
+    (if-not clean?
+      (util/fail (str "repo not clean: " status))
+      (println "repo is clean!"))
+    ))
 
