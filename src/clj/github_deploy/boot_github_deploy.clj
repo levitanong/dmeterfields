45aa2b456255cf2add9345ac98a98032eccabb3b
Levi Tan Ong <levi@byimplication.com>
2016-11-15 14:19:38 +0800

    adds more checks

--- src/clj/github_deploy/boot_github_deploy.clj
+++ src/clj/github_deploy/boot_github_deploy.clj
@@ -27,14 +27,17 @@
         (util/info "Repo is clean. Checking out master.")
         (git/git-checkout repo "master")
         (git/git-merge repo latest-commit)
-        (util/info "Merged. Pushing")
-        (with-programs [git]
-          (git "push")
-          (util/info "Pushed."))
         (if-not (clean? repo)
-          (util/fail (str "Something went wrong. Repo is not clean."
-                       (git/git-status repo)))
+          (util/fail (str "Merge failed: " (git/git-status repo)))
           (do
-            (git/git-checkout repo working-branch)
-            (util/info "Deploy successful.")))))))
+            (util/info "Merged. Pushing")
+            (with-programs [git]
+              (git "push")
+              (util/info "Pushed."))
+            (if-not (clean? repo)
+              (util/fail (str "Something went wrong. Repo is not clean."
+                           (git/git-status repo)))
+              (do
+                (git/git-checkout repo working-branch)
+                (util/info "Deploy successful.")))))))))
 
