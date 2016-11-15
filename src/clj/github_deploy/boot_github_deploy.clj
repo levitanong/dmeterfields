6701024e20be5b3440f9cf42b1d804d590a4afd9
Levi Tan Ong <levi@byimplication.com>
2016-11-15 15:27:22 +0800

    adds a guard against working in master

--- src/clj/github_deploy/boot_github_deploy.clj
+++ src/clj/github_deploy/boot_github_deploy.clj
@@ -22,24 +22,26 @@
         working-branch (git/git-branch-current repo)
         latest-commit (first (git/git-log repo))
         status (git/git-status repo)]
-    (if-not (clean? repo)
-      (util/fail (str "Repo not clean: " status "\n"))
-      (do
-        (util/info "Repo is clean. Checking out master. \n")
-        (git/git-checkout repo "master")
-        (git/git-merge repo latest-commit)
-        (if-not (clean? repo)
-          (util/fail (str "Merge failed: " (git/git-status repo) "\n"))
-          (do
-            (util/info "Merged. Moving contents of /target to /\n")
-            (sift :move {#"/target/*" "/"})
-            #_(with-programs [git]
-              (git "push")
-              (util/info "Pushed.\n"))
-            #_(if-not (clean? repo)
-              (util/fail (str "Something went wrong. Repo is not clean.\n"
-                           (git/git-status repo)))
-              (do
-                (git/git-checkout repo working-branch)
-                (util/info "Deploy successful.\n")))))))))
+    (if (= "master" working-branch)
+      (util/fail "Currently on master. Please switch to your working branch.\n")
+      (if-not (clean? repo)
+        (util/fail (str "Repo not clean: " status "\n"))
+        (do
+          (util/info "Repo is clean. Checking out master. \n")
+          (git/git-checkout repo "master")
+          (git/git-merge repo latest-commit)
+          (if-not (clean? repo)
+            (util/fail (str "Merge failed: " (git/git-status repo) "\n"))
+            (do
+              (util/info "Merged. Moving contents of /target to /\n")
+              (sift :move {#"/target/*" "/"})
+              #_(with-programs [git]
+                  (git "push")
+                  (util/info "Pushed.\n"))
+              #_(if-not (clean? repo)
+                  (util/fail (str "Something went wrong. Repo is not clean.\n"
+                               (git/git-status repo)))
+                  (do
+                    (git/git-checkout repo working-branch)
+                    (util/info "Deploy successful.\n"))))))))))
 
