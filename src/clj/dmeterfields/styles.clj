e50dde4ce83643da46caa6b44c81f42a3413b36b
Levi Tan Ong <levi@byimplication.com>
2016-11-15 15:11:46 +0800

    workify boot tasks

---
+++ src/clj/dmeterfields/styles.clj
@@ -0,0 +1,6 @@
+(ns dmeterfields.styles
+  (:require
+   [garden.color :refer [rgba]]))
+
+(def base
+  [:body {:background-color "#333"}])
--- src/cljs/dmeterfields/core.cljs
+++ src/cljs/dmeterfields/core.cljs
@@ -1,25 +1,19 @@
 (ns dmeterfields.core
-    (:require
-     [om.next :as om :refer-macros [defui]]
-     [om.dom :as dom]
-     [goog.dom :as gdom]))
-
-(enable-console-print!)
+  (:require
+   [om.next :as om :refer-macros [defui]]
+   [om.dom :as dom]
+   [goog.dom :as gdom]))
 
 (def app-state (atom {}))
-
 
 (defui Root
   Object
   (render [this]
-          (dom/div nil "om loaded")))
+    (dom/div nil "om loaded")))
 
 (def reconciler
   (om/reconciler {:state app-state}))
 
 (om/add-root! reconciler
-              Root
-              (gdom/getElement "app"))
-
-(defn main []
-  (println "reloading"))
+  Root
+  (gdom/getElement "app"))
